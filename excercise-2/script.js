73739396715d98c77643cca12eb3150ea6c6cd51
Mikaela Hedberg <mie@du.se>
2024-03-22 12:20:20 +0100

    ex2

---
+++ excercise-2/script.js
@@ -0,0 +1,69 @@
+const urlSCB =
+  'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningNy';
+
+const querySCB = {
+  query: [
+    {
+      code: 'Region',
+      selection: {
+        filter: 'vs:RegionKommun07',
+        values: [
+          '2021',
+          '2023',
+          '2026',
+          '2029',
+          '2031',
+          '2034',
+          '2039',
+          '2061',
+          '2062',
+          '2080',
+          '2081',
+          '2082',
+          '2083',
+          '2084',
+          '2085'
+        ]
+      }
+    },
+    {
+      code: 'ContentsCode',
+      selection: {
+        filter: 'item',
+        values: ['BE0101N1']
+      }
+    },
+    {
+      code: 'Tid',
+      selection: {
+        filter: 'item',
+        values: ['2023']
+      }
+    }
+  ],
+  response: {
+    format: 'px'
+  }
+};
+
+const request = new Request(url, {
+  method: 'POST',
+  body: JSON.stringify(querySCB)
+});
+
+fetch(request)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+const urlUN =
+  'https://unstats.un.org/SDGAPI/v1/sdg/DataAvailability/GetIndicatorsAllCountries';
+
+const requestUN = new Request(urlUN, {
+  method: 'POST',
+  body: 'dataPointType=1&countryId=0&natureOfData=all',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+});
+
+fetch(requestUN)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
