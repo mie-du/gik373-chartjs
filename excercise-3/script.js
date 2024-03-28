fa8334e80e1ce85b49e33e8f141ecb0da522316d
Mikaela Hedberg <mie@du.se>
2024-03-28 13:16:32 +0100

    ex3

---
+++ excercise-3/script.js
@@ -0,0 +1,130 @@
+const urlSCB =
+  'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningNy';
+
+const querySCB = {
+  query: [
+    {
+      code: 'Region',
+      selection: {
+        filter: 'vs:RegionKommun07',
+        values: ['2081']
+      }
+    },
+    {
+      code: 'ContentsCode',
+      selection: {
+        filter: 'item',
+        values: ['BE0101N1']
+      }
+    },
+    {
+      code: 'Tid',
+      selection: {
+        filter: 'item',
+        values: [
+          '2011',
+          '2012',
+          '2013',
+          '2014',
+          '2015',
+          '2016',
+          '2017',
+          '2018',
+          '2019',
+          '2020',
+          '2021',
+          '2022',
+          '2023'
+        ]
+      }
+    }
+  ],
+  response: {
+    format: 'JSON'
+  }
+};
+
+function printSCBChart(dataSCB) {
+  const years = dataSCB.data;
+  const labels = years.map((year) => year.key[1]);
+  console.log(labels);
+  const data = years.map((year) => year.values[0]);
+  console.log(data);
+  console.log(years);
+
+  const datasets = [
+    {
+      label: 'Befolkningsutveckling Borlänge',
+      data,
+      fill: false,
+      borderWidth: 2,
+      borderColor: 'hsla(250, 100%, 30%, 1)',
+      hoverBorderWidth: 4,
+      tension: 0.5
+    }
+  ];
+
+  new Chart(document.getElementById('scb'), {
+    type: 'line',
+    data: { labels, datasets }
+  });
+}
+
+const request = new Request(urlSCB, {
+  method: 'POST',
+  body: JSON.stringify(querySCB)
+});
+
+fetch(request)
+  .then((response) => response.json())
+  .then(printSCBChart);
+
+const urlUN =
+  'https://unstats.un.org/SDGAPI/v1/sdg/DataAvailability/GetIndicatorsAllCountries';
+
+const requestUN = new Request(urlUN, {
+  method: 'POST',
+  body: 'dataPointType=1&countryId=0&natureOfData=all',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+});
+
+fetch(requestUN)
+  .then((response) => response.json())
+  .then(printUNChart);
+
+function printUNChart(dataUN) {
+  const indicators = dataUN[6].indicators;
+  console.log(indicators);
+  const labels = indicators.map((indicator) => indicator.code);
+  console.log(labels);
+  const data = indicators.map((indicator) => indicator.percentage);
+  console.log(data);
+
+  const datasets = [
+    {
+      label: 'Uppfyllnad per indikator (%)',
+      data,
+      borderWidth: 2,
+      backgroundColor: 'hsla(70, 100%, 30%, 0.4)',
+      borderColor: 'hsla(70, 100%, 30%, 1)',
+      hoverBorderWidth: 4,
+      tension: 0.5
+    }
+  ];
+
+  new Chart(document.getElementById('un'), {
+    type: 'bar',
+    data: { labels, datasets },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Världens uppfyllnad av mål 7 - Hållbar energi för alla',
+          font: {
+            size: 24
+          }
+        }
+      }
+    }
+  });
+}
--- extra/script.js
+++ extra/script.js
@@ -1,49 +1,158 @@
-const urlSCB =
-  'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/LE/LE0102/LE0102L/LE0102L4';
+/* https://www.statistikdatabasen.scb.se/pxweb/sv/ssd/ */
+/* Låginkomst 0-17 */
 
-const querySCB = {
-  query: [
-    {
-      code: 'Kon',
-      selection: {
-        filter: 'item',
-        values: ['5+6']
+async function getLowIncomeData() {
+  const query = {
+    query: [
+      {
+        code: 'Alder',
+        selection: {
+          filter: 'item',
+          values: ['0-17']
+        }
+      },
+      {
+        code: 'Inkomststandard',
+        selection: {
+          filter: 'item',
+          values: ['lagStd']
+        }
+      },
+      {
+        code: 'BakgrundSvUt',
+        selection: {
+          filter: 'item',
+          values: ['TotalC']
+        }
+      },
+      {
+        code: 'UtbNivaForalder',
+        selection: {
+          filter: 'item',
+          values: ['30']
+        }
       }
-    },
+    ],
+    response: {
+      format: 'json'
+    }
+  };
+  const request = new Request(
+    'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/LE/LE0102/LE0102L/LE0102L5',
     {
-      code: 'Alder',
-      selection: {
-        filter: 'item',
-        values: ['0-17']
-      }
-    },
+      method: 'POST',
+      body: JSON.stringify(query)
+    }
+  );
+  const response = await fetch(request);
+  const stat = await response.json();
+  /* Sätter samam data i chart-egenskaper */
+  /* Plockar ur värden och skapar arrayer i format som chart.js förstår */
+  const labels = stat.data.map((obj) => obj.key[4]);
+  /* console.log(labels); */
+  const values = stat.data.map((obj) => obj.values[0]);
+  /* console.log(values); */
+  const datasets = [
     {
-      code: 'Inkomststandard',
-      selection: {
-        filter: 'item',
-        values: ['lagStd']
-      }
+      label: 'Antal barn under 17 boende i hushåll med låg inkomststandard',
+      data: values,
+      /* https://www.chartjs.org/docs/latest/charts/line.html#dataset-properties */
+      tension: 0.5,
+      backgroundColor: 'hsla(120, 100%, 50%, 0.5)',
+      borderColor: 'hsla(120, 100%, 50%, 0.5)',
+      /* https://www.chartjs.org/docs/latest/configuration/elements.html#types */
+      pointStyle: 'rectRot',
+      pointBackgroundColor: 'hsla(120, 100%, 70%, .6)',
+      pointHoverBackgroundColor: 'hsla(120, 100%, 30%)',
+      pointRadius: '6',
+      pointHoverRadius: '6'
+    }
+  ];
+  /* console.log(datasets); */
+  const canvas = document.getElementById('scb');
+  new Chart(canvas, {
+    type: 'line',
+    data: {
+      labels,
+      datasets
     },
-    {
-      code: 'Familjetyp',
-      selection: {
-        filter: 'item',
-        values: ['Total']
-      }
-    },
-    {
-      code: 'Antalsyskon',
-      selection: {
-        filter: 'item',
-        values: ['999']
+    options: {
+      plugins: {
+        title: {
+          text: 'Barn (0-17 år) i hushåll med låg inkomststandard',
+          display: true,
+          font: { size: 20 }
+        }
       }
     }
-  ],
-  response: {
-    format: 'px'
-  }
-};
+  });
+}
 
-fetch(urlSCB, { method: 'POST', body: JSON.stringify(querySCB) })
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+getLowIncomeData();
+
+async function getGoalByArea(goalId) {
+  const dataResult = await fetch(
+    'https://unstats.un.org/SDGAPI/v1/sdg/DataAvailability/GetCompareacrossgoalData',
+    {
+      method: 'POST',
+      body: `dataPointType=1&goals=${goalId}&natureOfData=All`,
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    }
+  ).then((response) => response.json());
+  const regions = dataResult.regions;
+  console.log(regions);
+  const labelResult = await fetch(
+    'https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false'
+  ).then((response) => response.json());
+  console.log(labelResult);
+  const goal = labelResult.filter((label) => label.code == goalId)[0];
+  const goalLabel = `Goal ${goal.code} - ${goal.title}`;
+
+  const labels = regions
+    .map((region) => region.region)
+    /* Ta bort världen ur datasetet */
+    .filter((region) => region !== 'World');
+
+  console.log(labels);
+  const data = regions.map((region) => region.goals[0].percentage);
+  console.log(data);
+  const datasets = [
+    {
+      label: 'Uppfyllnad av mål ' + goalId + ' (%)',
+      data,
+      backgroundColor: [
+        'hsla(0, 100%, 70%, .6)',
+        'hsla(20, 100%, 70%, .6)',
+        'hsla(40, 100%, 70%, .6)',
+        'hsla(60, 100%, 70%, .6)',
+        'hsla(80, 100%, 70%, .6)',
+        'hsla(100, 100%, 70%, .6)',
+        'hsla(120, 100%, 70%, .6)',
+        'hsla(140, 100%, 70%, .6)',
+        'hsla(160, 100%, 70%, .6)',
+        'hsla(170, 100%, 70%, .6)',
+        'hsla(190, 100%, 70%, .6)'
+      ],
+      borderRadius: 10
+    }
+  ];
+  const canvas = document.getElementById('un');
+  new Chart(canvas, {
+    type: 'bar',
+    data: {
+      labels,
+      datasets
+    },
+    options: {
+      plugins: {
+        title: {
+          text: goalLabel,
+          display: true,
+          font: { size: 20 }
+        }
+      }
+    }
+  });
+}
+
+getGoalByArea(1);
