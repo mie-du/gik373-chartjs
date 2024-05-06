0d77aef32263f823c1531c02d1168b0d07541a37
Mikaela Hedberg <mie@du.se>
2024-05-06 12:02:14 +0200

    label

--- script.js
+++ script.js
@@ -280,7 +280,7 @@ async function getRegistredVehicleData() {
     options: {
       plugins: {
         title: {
-          text: 'Typer av nyregistrerade personbilar, april 2024',
+          text: 'Typer av nyregistrerade personbilar, mars 2024',
           display: true,
           font: fonts.title
         }
